9690938886a66da966f90620ffa8fc963ae35f80
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-03-13 02:08:14 -0600

    add some changes...

--- app.js
+++ app.js
@@ -1,92 +1,121 @@
-const title = document.querySelector(".title");
-title.innerHTML = "Sapphire Fly";
-
-// Create context 2D
+//* Creamos el Canvas con contexto 2D
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// Bird params
-const bird = {
+// creamos la imagen de dinoImage
+const dinoImage = new Image();
+dinoImage.src =
+  "https://bestanimations.com/media/dinosaur-art/2131955109dinosaur-animation-34.gif";
+dinoImage.style.display = "block";
+
+let isDead = false;
+
+//* Objeto Dino
+let dino = {
 	x: 50,
-	y: canvas.height / 2,
-	width: 20,
-	height: 20,
+	y: 100,
+	width: 120,
+	height: 120,
 	speed: 1,
-	gravity: 0.2,
+	gravity: 0.15,
 	jump: 6,
-	onGround: true,
+	src: "https://bestanimations.com/media/dinosaur-art/2131955109dinosaur-animation-34.gif",
 };
 
-// Obstacules array
-const obstacules = [];
-
-// Obstacules params
-const obstaculesProps = {
-	obstaculeHeight: 70,
-	obstaculeWidth: 50,
-	obstaculeGap: 50,
-	obstaculeSpeed: 2,
-	lastObstacule: canvas.width,
-};
-
-document.addEventListener("keydown", (evt) => {
-	if (evt.keyCode === 32 && bird.onGround) {
-		jumpBird();
-	}
+document.addEventListener("click", () => {
+	loop();
+	//* Salta al presionar la tecla "espacio"
+	document.addEventListener("keydown", (evt) => {
+		if (evt.keyCode === 32) {
+		// salta al presionar la tecla "espacio"
+			jump();
+		}
+	});
 });
 
+//* Función de dibujado de Canvas
 const draw = () => {
-	// clear the canvas
+	// borrar el contenido del Canvas
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	// draw background
-	ctx.fillStyle = "lightblue";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	// draw the bird
-	ctx.fillStyle = "red";
-	ctx.fillRect(bird.x, bird.y, bird.width, bird.height);
+	// renderizamos el dino
+	ctx.drawImage(dinoImage, dino.x, dino.y, dino.width, dino.height);
 };
 
-const jumpBird = () => {
-	bird.onGround = "false";
-	bird.speed = -bird.jump;
-	bird.onGround = "true";
+const restart = () => {
+	document.removeEventListener("click", restart);
+
+	document.addEventListener("keydown", (evt) => {
+		if (evt.keyCode === 32) {
+			isDead = false;
+			loop();
+			const bg = document.querySelectorAll(".bg");
+			bg.forEach((bg) => {
+				if (bg[0]) {
+					bg.stye.animation = "bgMove1 8s linear infinite";
+				}
+				bg.stye.animation = "bgMove2 8s linear infinite";
+			});
+		}
+	});
 };
 
-//* Función de actualización de juego
+const gameOver = () => {
+	const bg = document.querySelectorAll(".bg");
+	bg.forEach((bg) => (bg.style.animation = "none"));
+
+	cancelAnimationFrame(loop);
+	isDead = true;
+	dino = {
+		x: 50,
+		y: 100,
+		width: 120,
+		height: 120,
+		speed: 1,
+		gravity: 0.15,
+		jump: 6,
+	};
+
+	document.addEventListener("click", restart);
+};
+
+//* Función de salto de Dino
+const jump = () => {
+	dino.speed = -dino.jump;
+};
+
+//* Función de actualización
 const update = () => {
-	// Actualiza la velocidad de caída del pájaro
-	bird.speed += bird.gravity;
+	// Actualiza la velocidad de caída del dino
+	dino.speed += dino.gravity;
 
-	// Actualiza la posición del pájaro en función de su velocidad de salto
-	bird.y += bird.speed;
+	// Actualiza la posición del dino en función de su velocidad de salto
+	dino.y += dino.speed;
 
-	// Limita la posición del pájaro a los bordes superior e inferior del lienzo
-	if (bird.y < 0) {
-		bird.y = 0;
-		bird.velocity = 0;
-	} else if (bird.y + bird.height > canvas.height) {
-		bird.y = canvas.height - bird.height;
-		bird.velocity = 0;
+	// Limita la posición del dino a los bordes superior e inferior del lienzo
+	if (dino.y < 0) {
+		dino.y = 0;
+		dino.velocity = 0;
+	} else if (dino.y + dino.height > canvas.height) {
+		dino.y = 100;
+		gameOver();
 	}
 };
 
-
-//* Loop function
+//TODO Función loop
 const loop = () => {
-	// update function
-	update();
+	if (!isDead) {
+		// actualizar la posición del dino y del Canvas
+		update();
 
-	// draw elements of canvas
-	draw();
+		// renderizar fondo de Canvas y el dino
+		draw();
 
-	// Update bird position
-	bird.speed += bird.gravity;
-	bird.y += bird.speed;
-	
-	// Solicita la siguiente actualización de animación
-	requestAnimationFrame(loop);
+		// actualizar la posición del dino
+		dino.speed += dino.gravity;
+		dino.y += dino.speed;
+
+		// solicita la siguiente actualización de animación
+		requestAnimationFrame(loop);
+	}
 };
-
-loop();
